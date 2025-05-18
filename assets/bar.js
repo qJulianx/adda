b66e0b7ce83471bd49694e17ce94a269184f21a6
qJulianx <99652819+qJulianx@users.noreply.github.com>
2025-05-18 20:17:56 +0200

    Update bar.js

--- assets/bar.js
+++ assets/bar.js
@@ -2,7 +2,7 @@
 var params = new URLSearchParams(window.location.search);
 
 function sendTo(url){
-    location.href = `/xsad/${url}?` + params;
+    location.href = `/adda/${url}?` + params;
 }
 
 document.querySelectorAll(".bottom_element_grid").forEach((element) => {
